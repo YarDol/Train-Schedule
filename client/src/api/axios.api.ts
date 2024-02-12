a8280d027d9e88f2a9db810bd0b872b6a6530170
YarDol <yardolhushyn@gmail.com>
2024-02-12 09:52:13 +0200

    server url update

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getTokenFromLocalStorage } from "../utils/localstorage";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/',
+    // baseURL: 'http://localhost:5000/api/',
+    baseURL: process.env.REACT_APP_SERVER_URL,
     headers: {
         Authorization: 'Bearer ' + getTokenFromLocalStorage() || ''
     }
